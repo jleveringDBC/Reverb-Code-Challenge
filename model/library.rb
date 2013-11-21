744d309003724b0d2c63ffd4126ba8fd7a7286fd
Joe Levering <joseph.w.levering@gmail.com>
2013-11-21 16:11:58 -0600

    Library initializes and adds records appropriately.

--- model/library.rb
+++ model/library.rb
@@ -1,3 +1,15 @@
+require_relative 'record'
+
 class Library
+
+  attr_reader :records
+
+  def initialize()
+    @records = []
+  end
+
+  def add_record(record)
+    @records << record
+  end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,5 +3,23 @@ require_relative '../model/library'
 
 describe Library do
 
+  let(:valid_library) { Library.new() }
+
+  describe '#add_record' do
+    it 'appropriately adds records' do
+      valid_library.add_record(Record.new({}))
+      valid_library.add_record(Record.new({}))
+      expect(valid_library.records.count).to eq 2
+    end
+  end
+
+  describe '#sort_by_gender' do
+  end
+
+  describe '#sort_by_birth_date' do
+  end
+
+  describe '#sort_by_last_name' do
+  end
 
 end
