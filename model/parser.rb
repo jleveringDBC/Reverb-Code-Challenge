44bf1661eed3e6fe48da46a6b1a119c2d4888088
Joe Levering <joseph.w.levering@gmail.com>
2013-11-25 15:56:41 -0600

    Added functionality to #process for Parser to correctly handle comma and space delimited records. Needs major refactoring.

--- model/parser.rb
+++ model/parser.rb
@@ -29,9 +29,31 @@ class Parser
                                           birth_date: birth_date,
                                           favorite_color: favorite_color } ) )
           elsif line.match(/,/)
-            record_info = line.match(//)
+            record_info = line.match(/(.*), (.*), (.*), (.*), (.*)/)
+            first_name = record_info[2]
+            last_name = record_info[1]
+            gender = record_info[3]
+            birth_date = Date.strptime(record_info[5], "%m/%d/%Y")
+            favorite_color = record_info[4]
+            @library.add_record(Record.new( { first_name: first_name,
+                                          last_name: last_name,
+                                          gender: gender,
+                                          birth_date: birth_date,
+                                          favorite_color: favorite_color } ) )
           else
-            record_info = line.match(//)
+            record_info = line.match(/(.*) (.*) (.*) (.*) (.*) (.*)/)
+            first_name = record_info[2]
+            last_name = record_info[1]
+            middle_initial = record_info[3]
+            record_info[4] == "M" ? gender = "Male" : gender = "Female"
+            birth_date = Date.strptime(record_info[5], "%m-%d-%Y")
+            favorite_color = record_info[6]
+            @library.add_record(Record.new( { first_name: first_name,
+                                          last_name: last_name,
+                                          middle_initial: middle_initial,
+                                          gender: gender,
+                                          birth_date: birth_date,
+                                          favorite_color: favorite_color } ) )
           end
         end
       end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -25,15 +25,42 @@ describe Parser do
       expect(parser.process("pipe.txt", "comma.txt")).to_not raise_error
     end
 
+    def post_process_expectations(records)
+      expect(records.count).to eq(3)
+      expect(records.last).to be_a(Record)
+    end
+
     context 'when passed pipe delimited records' do
+
       it 'saves each line as a record in the library' do
         parser.process('pipe.txt')
         records = parser.library.records
-        expect(records.count).to eq(3)
-        expect(records.last).to be_a(Record)
+        post_process_expectations(records)
         expect(records.last.last_name).to eq("Bouillon")
         expect(records.last.gender).to eq("Male")
         expect(records.last.birth_date).to eq(Date.strptime("6-3-1975", "%m-%d-%Y"))
+      end
+    end
+
+    context 'when passed comma delimited records' do
+      it 'saves each line as a record in the library' do
+        parser.process('comma.txt')
+        records = parser.library.records
+        post_process_expectations(records)
+        expect(records.last.last_name).to eq("Kelly")
+        expect(records.last.gender).to eq("Female")
+        expect(records.last.birth_date).to eq(Date.strptime("7/12/1959", "%m/%d/%Y"))
+      end
+    end
+
+    context 'when passed space delimited records' do
+      it 'saves each line as a record in the library' do
+        parser.process('space.txt')
+        records = parser.library.records
+        post_process_expectations(records)
+        expect(records.last.last_name).to eq("Seles")
+        expect(records.last.gender).to eq("Female")
+        expect(records.last.birth_date).to eq(Date.strptime("12-2-1973", "%m-%d-%Y"))
       end
     end
   end
