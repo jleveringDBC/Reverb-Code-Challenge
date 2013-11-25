9f2fc9cc054e059b35f3d473574c72475dbdc675
Joe Levering <joseph.w.levering@gmail.com>
2013-11-25 13:37:05 -0600

    Parser initializes with passing tests

--- model/parser.rb
+++ model/parser.rb
@@ -1,3 +1,13 @@
 class Parser
 
+  attr_reader :library
+
+  def initialize(library = Library.new)
+    if library.class != Library
+      raise ArgumentError, 'Argument must be a Library'
+    else
+      @library = library
+    end
+  end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,7 +1,20 @@
 require_relative 'spec_helper'
 require_relative '../model/parser'
+require_relative '../model/library'
 
 describe Parser do
 
+  describe '.initialize' do
+    it 'takes a library as an argument' do
+      expect(Parser.new(Library.new)).to be_a(Parser)
+    end
 
+    it 'does not accept a non-Library argument' do
+      expect{Parser.new("string")}.to raise_error(ArgumentError, 'Argument must be a Library')
+    end
+
+    it 'initializes with empty Library without argument' do
+      expect(Parser.new.library).to be_a(Library)
+    end
+  end
 end
