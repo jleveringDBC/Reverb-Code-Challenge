f1602c680c9e7dd7a420e49cb61f40d95f9e380d
Joe Levering <joseph.w.levering@gmail.com>
2013-11-25 15:33:26 -0600

    Added #process to Parser. Added first-run parse functionality for pipe delimited files.

--- model/parser.rb
+++ model/parser.rb
@@ -10,4 +10,32 @@ class Parser
     end
   end
 
+  def process(*filenames)
+    filenames.each do |filename|
+      File.open(filename, 'r') do |f|
+        while (line = f.gets)
+          if line.match(/\|/)
+            record_info = line.match(/(.*) \| (.*) \| (.*) \| (.*) \| (.*) \| (.*)/)
+            first_name = record_info[2]
+            last_name = record_info[1]
+            middle_initial = record_info[3]
+            record_info[4] == "M" ? gender = "Male" : gender = "Female"
+            birth_date = Date.strptime(record_info[6], "%m-%d-%Y")
+            favorite_color = record_info[5]
+            @library.add_record(Record.new( { first_name: first_name,
+                                          last_name: last_name,
+                                          middle_initial: middle_initial,
+                                          gender: gender,
+                                          birth_date: birth_date,
+                                          favorite_color: favorite_color } ) )
+          elsif line.match(/,/)
+            record_info = line.match(//)
+          else
+            record_info = line.match(//)
+          end
+        end
+      end
+    end
+  end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,6 +4,8 @@ require_relative '../model/library'
 
 describe Parser do
 
+  let(:parser) { Parser.new }
+
   describe '.initialize' do
     it 'takes a library as an argument' do
       expect(Parser.new(Library.new)).to be_a(Parser)
@@ -14,7 +16,25 @@ describe Parser do
     end
 
     it 'initializes with empty Library without argument' do
-      expect(Parser.new.library).to be_a(Library)
+      expect(parser.library).to be_a(Library)
+    end
+  end
+
+  describe '#process' do
+    it 'takes a variable number of arguments' do
+      expect(parser.process("pipe.txt", "comma.txt")).to_not raise_error
+    end
+
+    context 'when passed pipe delimited records' do
+      it 'saves each line as a record in the library' do
+        parser.process('pipe.txt')
+        records = parser.library.records
+        expect(records.count).to eq(3)
+        expect(records.last).to be_a(Record)
+        expect(records.last.last_name).to eq("Bouillon")
+        expect(records.last.gender).to eq("Male")
+        expect(records.last.birth_date).to eq(Date.strptime("6-3-1975", "%m-%d-%Y"))
+      end
     end
   end
 end
