85cda08d4fbc1a35ab40c3d5aa367c8a82533686
Joe Levering <joseph.w.levering@gmail.com>
2013-11-25 16:52:09 -0600

    Refactored #process for Parser and associated tests.

--- model/parser.rb
+++ model/parser.rb
@@ -15,49 +15,44 @@ class Parser
       File.open(filename, 'r') do |f|
         while (line = f.gets)
           if line.match(/\|/)
+            delimiter = "pipe"
             record_info = line.match(/(.*) \| (.*) \| (.*) \| (.*) \| (.*) \| (.*)/)
-            first_name = record_info[2]
-            last_name = record_info[1]
-            middle_initial = record_info[3]
-            record_info[4] == "M" ? gender = "Male" : gender = "Female"
-            birth_date = Date.strptime(record_info[6], "%m-%d-%Y")
-            favorite_color = record_info[5]
-            @library.add_record(Record.new( { first_name: first_name,
-                                          last_name: last_name,
-                                          middle_initial: middle_initial,
-                                          gender: gender,
-                                          birth_date: birth_date,
-                                          favorite_color: favorite_color } ) )
           elsif line.match(/,/)
+            delimiter = "comma"
             record_info = line.match(/(.*), (.*), (.*), (.*), (.*)/)
-            first_name = record_info[2]
-            last_name = record_info[1]
-            gender = record_info[3]
-            birth_date = Date.strptime(record_info[5], "%m/%d/%Y")
-            favorite_color = record_info[4]
-            @library.add_record(Record.new( { first_name: first_name,
-                                          last_name: last_name,
-                                          gender: gender,
-                                          birth_date: birth_date,
-                                          favorite_color: favorite_color } ) )
           else
+            delimiter = "space"
             record_info = line.match(/(.*) (.*) (.*) (.*) (.*) (.*)/)
-            first_name = record_info[2]
-            last_name = record_info[1]
-            middle_initial = record_info[3]
-            record_info[4] == "M" ? gender = "Male" : gender = "Female"
-            birth_date = Date.strptime(record_info[5], "%m-%d-%Y")
-            favorite_color = record_info[6]
-            @library.add_record(Record.new( { first_name: first_name,
-                                          last_name: last_name,
-                                          middle_initial: middle_initial,
-                                          gender: gender,
-                                          birth_date: birth_date,
-                                          favorite_color: favorite_color } ) )
           end
+          record_variables = parse_record(record_info, delimiter)
+          @library.add_record(Record.new(record_variables))
         end
       end
     end
+  end
+
+  private
+
+  def parse_record(record_info, delimiter)
+    first_name = record_info[2]
+    last_name = record_info[1]
+    if delimiter == "pipe"
+      middle_initial = record_info[3]
+      record_info[4] == "M" ? gender = "Male" : gender = "Female"
+      birth_date = Date.strptime(record_info[6], "%m-%d-%Y")
+      favorite_color = record_info[5]
+    elsif delimiter == "comma"
+      middle_initial = nil
+      gender = record_info[3]
+      birth_date = Date.strptime(record_info[5], "%m/%d/%Y")
+      favorite_color = record_info[4]
+    elsif delimiter == "space"
+      middle_initial = record_info[3]
+      record_info[4] == "M" ? gender = "Male" : gender = "Female"
+      birth_date = Date.strptime(record_info[5], "%m-%d-%Y")
+      favorite_color = record_info[6]
+    end
+    { first_name: first_name, last_name: last_name, middle_initial: middle_initial, gender: gender, birth_date: birth_date, favorite_color: favorite_color }
   end
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -22,46 +22,38 @@ describe Parser do
 
   describe '#process' do
     it 'takes a variable number of arguments' do
-      expect(parser.process("pipe.txt", "comma.txt")).to_not raise_error
+      expect(parser.process("pipe_test.txt", "comma_test.txt")).to_not raise_error
     end
 
     def post_process_expectations(records)
       expect(records.count).to eq(3)
       expect(records.last).to be_a(Record)
+      expect(records.last.last_name).to eq("Bouillon")
+      expect(records.last.gender).to eq("Male")
+      expect(records.last.birth_date).to eq(Date.strptime("6-3-1975", "%m-%d-%Y"))
     end
 
     context 'when passed pipe delimited records' do
 
       it 'saves each line as a record in the library' do
-        parser.process('pipe.txt')
-        records = parser.library.records
-        post_process_expectations(records)
-        expect(records.last.last_name).to eq("Bouillon")
-        expect(records.last.gender).to eq("Male")
-        expect(records.last.birth_date).to eq(Date.strptime("6-3-1975", "%m-%d-%Y"))
+        parser.process('pipe_test.txt')
+        post_process_expectations(parser.library.records)
       end
     end
 
     context 'when passed comma delimited records' do
       it 'saves each line as a record in the library' do
-        parser.process('comma.txt')
-        records = parser.library.records
-        post_process_expectations(records)
-        expect(records.last.last_name).to eq("Kelly")
-        expect(records.last.gender).to eq("Female")
-        expect(records.last.birth_date).to eq(Date.strptime("7/12/1959", "%m/%d/%Y"))
+        parser.process('comma_test.txt')
+        post_process_expectations(parser.library.records)
       end
     end
 
     context 'when passed space delimited records' do
       it 'saves each line as a record in the library' do
-        parser.process('space.txt')
-        records = parser.library.records
-        post_process_expectations(records)
-        expect(records.last.last_name).to eq("Seles")
-        expect(records.last.gender).to eq("Female")
-        expect(records.last.birth_date).to eq(Date.strptime("12-2-1973", "%m-%d-%Y"))
+        parser.process('space_test.txt')
+        post_process_expectations(parser.library.records)
       end
     end
   end
+
 end
