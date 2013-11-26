d9a12abc388bbb70be8f8503e4657347235a6aca
Joe Levering <joseph.w.levering@gmail.com>
2013-11-26 13:36:46 -0600

    Tweaked birth_date sorting for Library.

--- model/library.rb
+++ model/library.rb
@@ -19,7 +19,9 @@ class Library
   end
 
   def sort_by_birth_date!
-    @records.sort! { |a, b| a.birth_date <=> b.birth_date }
+    @records.sort! { |a, b| a.birth_date == b.birth_date ?
+                      a.last_name <=> b.last_name :
+                      a.birth_date <=> b.birth_date }
   end
 
   def sort_by_last_name!
